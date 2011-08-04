e496285e113c522ab68cd3b386cdc355374377a9
whitered <dmitry.zhelnin@gmail.com>
2011-08-04 22:54:57 +0400

    3.1.0.rc4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsNew::Application.config.secret_token = '2577f9675f1f5cec6b08ca24cc34467f8969a23115b798fcf429ae36e9fa636adf8420b29f4fe89b6933ddd560680851406c31fae5bda31a7d81857c9f453a66'
+RailsNew::Application.config.secret_token = '1c3f7e2ffb2b8d3739827d89d63143af936c64dfc519387053433d70abe9ef0ca3ab3fa407c27dfb20533d7a5bc9ba457357593ead99de16a903c92d66e11e43'
