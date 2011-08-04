b861678ca44baa165850e0bdf39b6c9a64d49504
whitered <dmitry.zhelnin@gmail.com>
2011-08-04 21:23:34 +0400

    2.1.0

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'd4a223b698e0290d4093df44c542e960'
+  protect_from_forgery # :secret => '713c3d09a034ba32d1144b1a4fc91bde'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
 end
